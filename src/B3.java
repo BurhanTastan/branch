b6e2b060458f3d1a1ae6a5d1a1e53d6496e0c4dc
Burhan Tastan <119176022+BurhanTastan@users.noreply.github.com>
2022-12-23 20:28:53 +0100

    branch v3

---
+++ src/B3.java
@@ -0,0 +1,17 @@
+public class B3 {
+    public static void main(String[] args) {
+
+        /*
+        Projemizde onemli bir degisiklik yapmak istiyoruz. Bu degisiklige 10 gun
+        calisacagiz. Degisikligi yapmaya devam ederken orjinal projemizi riske atmamak
+        ve eger son kullanici kullaniyorsa olusabilecek sorunlari engellemek icin,
+
+        orjinal proje uzerinde calismak yerine yeni bir branch olusturup,
+        o branch'de degisiklikleri yapariz
+
+        Calisma sonucunda istenen hedefe ulasabilirsek olusturdugumuz branch i asil
+        branche kopyalariz.
+
+         */
+}
+}
